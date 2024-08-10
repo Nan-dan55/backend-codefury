2a7c5df37b64270f990eb0fc6c38ec889abb6e0d
NANDAN RAJEEV <nandanrajeev@NANDANs-MacBook-Air.local>
2024-08-10 13:45:49 +0530

    added database

--- index.js
+++ index.js
@@ -10,6 +10,34 @@ import env from "dotenv";
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
+env.config();
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+db.connect();
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
