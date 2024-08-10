a6a1293f094b633d04b50b1c6844db80b380b327
NANDAN RAJEEV <nandanrajeev@NANDANs-MacBook-Air.local>
2024-08-10 13:26:28 +0530

    complete backend

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+import session from "express-session";
+import env from "dotenv";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
